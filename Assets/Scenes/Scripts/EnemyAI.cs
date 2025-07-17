9e4f27838c2cff933f874c3e47026408e2d1009c
Faith Lim <faithlimk0901@gmail.com>
2025-07-17 18:29:34 +0800

    Commit 1

---
+++ Assets/Scenes/Scripts/EnemyAI.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private int damage;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
+
+    private float cooldownTimer = Mathf.Infinity;
+    //private Health playerhealth;
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+        if (PlayerInSight())
+        {
+
+            if (cooldownTimer >= attackCooldown)
+            {
+                cooldownTimer = 0;
+            }
+        }
+    }
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+        new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z), 0, Vector2.left, 0, playerLayer);
+        
+        //if (hit.collider != null)
+        //{
+            //playerHealth = hit.transform.GetComponent<Health>();
+        //}
+
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+
+    private void DamagePlayer()
+    {
+        //if (PlayerInSight())
+        //{
+            //playerHealth.TakeDamage(damage);
+        //}
+    }
+
+}
